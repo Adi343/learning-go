d07d6705d7bd1fb4dd87af94f3a7050f4dd9e8ff
Adi343 <53225414+Adi343@users.noreply.github.com>
2022-07-14 19:59:19 +0530

    Basic data types

---
+++ dataTypes.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main(){
+	// Basic data types
+	var num int = 4
+	const pI float32 = 3.14;
+	const s string = "hello world"
+	var isBoolean bool = true
+
+	fmt.Println("num is ",num)
+	fmt.Println("pI is ",pI)
+	fmt.Println("s is",s)
+	fmt.Println("isBoolean is",isBoolean)
+
+}

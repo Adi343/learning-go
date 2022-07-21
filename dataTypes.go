5e7c902057748c9d8d5d269498a51453589cb6d7
Adi343 <53225414+Adi343@users.noreply.github.com>
2022-07-21 19:47:55 +0530

    Added arrays and structs to dataTypes.go

--- dataTypes.go
+++ dataTypes.go
@@ -13,4 +13,22 @@ func main(){
 	fmt.Println("s is",s)
 	fmt.Println("isBoolean is",isBoolean)
 
+
+	var nums [3]int;
+	fmt.Println(nums[1])
+
+	fabonacci := [5]int{0,1,1,2,3}
+
+	fmt.Println(fabonacci)
+	fmt.Println(len(fabonacci))
+
+	type car struct{
+		engine string
+		hp int
+	}
+
+	mustang := car{engine:"v8"}
+	fmt.Println(mustang)
+	fmt.Println(mustang.engine)
+
 }
